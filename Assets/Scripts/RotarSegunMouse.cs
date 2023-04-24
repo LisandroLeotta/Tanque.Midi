47cb8ec6658e5f21fb8a2e9fc0a31bc4d764cff7
Damian <119361188+TukiBeanGames@users.noreply.github.com>
2023-04-24 10:56:07 -0300

    Agregue la rotacion del cañon del tanque y la rotacion de los enemigos para que sigan al tanque

---
+++ Assets/Scripts/RotarSegunMouse.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Descripcion:
+ * Se encarga de rotar al objeto que lo contega,
+ * segun el mouse.
+ */
+public class RotarSegunMouse : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position); //toma posicion del jugador.
+        Vector3 mouseOneScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition); //toma posicion del mouse.
+
+        Vector3 direction = mouseOneScreen - positionOnScreen; //calcula el vector o la distancia entre ambos puntos.
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f; //calcula los radianes y lo cmobierte a angulo.
+        transform.rotation = Quaternion.Euler(0, -angle, 0); //Modifica la rotacion.
+    }
+}
---
+++ Assets/Scripts/RotarSegunObjetivo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Descripcion:
+ * Se encarga de rotar al objeto que lo contega,
+ * segun la posicion de un objetivo asignado.
+ */
+public class RotarSegunObjetivo : MonoBehaviour
+{
+    [SerializeField] private Transform objetive;
+
+    private void Update()
+    {
+        Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position); //toma posicion del jugador.
+        Vector3 objetiveOneScreen = (Vector2)Camera.main.WorldToViewportPoint(objetive.position); //toma posicion del objetivo.
+
+        Vector3 direction = objetiveOneScreen - positionOnScreen; //calcula el vector o la distancia entre ambos puntos.
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f; //calcula los radianes y lo cmobierte a angulo.
+        transform.rotation = Quaternion.Euler(0, -angle, 0); //Modifica la rotacion.
+    }
+}
